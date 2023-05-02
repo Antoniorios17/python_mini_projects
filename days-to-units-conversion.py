b31b4d08b564b5d759ebdc1b93f63d8be11105cd
AntonioRios <63029609+Antoniorios17@users.noreply.github.com>
2023-05-02 17:26:55 -0400

    Update days-to-units-conversion.py

--- days-to-units-conversion.py
+++ days-to-units-conversion.py
@@ -27,6 +27,6 @@ def validate_and_execute():
 user_input=""
 while user_input != "exit": 
     user_input=input("Hey user, enter the number of days and conversion unit! \n --> ")
-    days_and_unit = user_input.split(":")
+    days_and_unit = user_input.split(" ")
     days_and_unit_dictionary = {"days": days_and_unit[0], "unit": days_and_unit[1]}   
     validate_and_execute()
