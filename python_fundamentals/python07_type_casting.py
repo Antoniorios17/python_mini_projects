5b95e4bf2d0f5932ae3b9d474098d65ed9ee5ccd
Antonio Rios <Antoniorioscarballo17@gmail.com>
2023-06-26 00:49:58 -0400

    update type casting

---
+++ python_fundamentals/python07_type_casting.py
@@ -0,0 +1,14 @@
+#typecasting is to convert from one datatype to another
+#examples
+
+#from integer to string
+#from string to integer
+#from string to float
+
+#there are three builtin functions to do typecasting
+#int()
+#float()
+#str()
+
+#int
+
