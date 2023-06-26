4f41f673826dcb985a87fbb2e852d141cc51b4bc
Antonio Rios <Antoniorioscarballo17@gmail.com>
2023-06-25 21:44:51 -0400

    update naming scheme

---
+++ python_fundamentals/python04_data_types.py
@@ -0,0 +1,51 @@
+#Integers
+
+#Integers are number with no decimal parts
+#such as -5, 4, 0, 5, 7
+
+#Example of Integers
+UserAge = 20
+mobile_number= 5551234567
+
+#Float
+#Float refers to the numbers that have decimal parts
+#such as 1.2213, 21.2232, 32.3
+
+#Example of Float
+
+user_height = 1.4
+user_weight = 123.2
+
+#String 
+#String refers to text
+
+#To declare a string you can use single quotes '' or double quotes ""
+
+#Example of string
+
+username = "Peter"
+user_spouse = "Emma"
+user_age = "30"
+
+#user_age is a string because of the quotes,
+#if the quotes were remove you would have an integer 
+
+#We can combine strings by using the concatenate sign (+)
+#Example
+# "Peter" + "Pan" = PeterPan
+
+#Built-in String Functions
+#Python Includes functions to manipulate strings
+
+#upper() capitalizes all letters in a string
+#"peter".upper() = "PETER"
+
+#Formating Strings using the % Operator
+#The syntax for using the operator is 
+
+#"string to be formatted" %(values or variables to be inserted into string, separated by commas)
+
+
+
+
+
