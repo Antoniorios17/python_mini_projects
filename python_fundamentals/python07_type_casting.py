ae2a3e4e90acef22842c1850a2ce34b89945558a
Antonio Rios <Antoniorioscarballo17@gmail.com>
2023-06-26 11:40:59 -0400

    typcasting notes

--- python_fundamentals/python07_type_casting.py
+++ python_fundamentals/python07_type_casting.py
@@ -10,5 +10,23 @@
 #float()
 #str()
 
-#int
+#int() Function
+#takes and appropiate string and converts it to an integer such as "1"
+#Changing float to an integer:
+# int(5.1231) will remove all the decimals and return 5
+
+#float() Function
+#Takes an integer or appropiate string and converts it to a float
+#Example:
+#float(2) or float("2") will return 2.0
+#float("2.156315") will return 2.156315 without the quotation marks
+
+#str() Function
+#it converts and appropiate string or integer to a string
+#example:
+#str(2.1) will return "2.1"
+
+
+
+
 
