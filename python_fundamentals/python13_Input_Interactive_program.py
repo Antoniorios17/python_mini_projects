92ffb00223d936d0d7448591c13da0dee57df531
Antonio Rios <Antoniorioscarballo17@gmail.com>
2023-06-26 16:01:10 -0400

    update notes

---
+++ python_fundamentals/python13_Input_Interactive_program.py
@@ -0,0 +1,16 @@
+# updated hello world program
+
+my_name = input("please enter your name: ")
+my_age = input("Please enter your age: ")
+
+print("Hello World, my name is", my_name, "and I am", my_age, "years old")
+
+# there are alternative ways to ask for the age after getting the name
+# from
+# my_age = input("what about your age: ")
+# #to
+# my_age = input("Hi %s, what about your age: " %(my_name) )
+# #or
+# my_age = input("Hi {}, what about your age: ".format(my_name))
+
+
