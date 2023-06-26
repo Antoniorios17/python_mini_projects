70231d7f5183512dcbd845bf247f8ec685ac7db8
Antonio Rios <Antoniorioscarballo17@gmail.com>
2023-06-26 17:03:59 -0400

    updated notes

---
+++ python_fundamentals/python17_Conditional_Statements.py
@@ -0,0 +1,50 @@
+# Conditional Statements
+
+# All flow control tools involve evaluating a condition statement
+#most common one is == 
+
+#==
+# we use it to compare if two variables are the same
+# example
+# x == y
+# you are asking the program to check if the value of x is equal to y
+#if the condition is met then the statement will be "True"
+# else the statement will be "False"
+
+# not equal !=
+5 != 2
+
+#greater than >
+5 > 2
+
+# smaller than <
+5 < 2
+
+# greater than or equal to:
+5 >= 2
+4 >= 4
+
+# smaller than or equal to:
+2 <= 2
+3 <= 5
+
+# we also have logical operators
+# and , or, not
+
+# and
+# and will return True is all conditions are true
+5==5 and 2>1
+
+# or 
+# or will operator returns True if at least one condition is met
+5 > 2 or 7 > 10 or 3 == 2
+
+# not
+# not operator returns True if the condition after the not keyword is false
+# else it will return false
+
+# the statement 
+not 2 > 5 
+#will return True since 2 is not greater than 5
+
+
---
+++ python_fundamentals/python18_If_statement.py
@@ -0,0 +1,22 @@
+#if statment
+
+#if statement is one of the most common control flows statement
+
+#it allow the program to evaluate if a condition is met
+# and perform an appropiate action based on the result of the evaluation
+
+#example
+# if condition 1 is met:  
+#     do A    
+# elif condition 2 is met:
+#     do B
+# elif condition 3 is met:
+#     do C
+# elif condition 4 is met:
+#     do D
+# else:
+#     do E
+
+# elif means else if
+#you can have as many elif as you like
+
