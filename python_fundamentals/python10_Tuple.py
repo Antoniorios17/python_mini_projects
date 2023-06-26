89d1dda5e2a5b6d8e3780ebc1eb7fbd6c4a2c40e
Antonio Rios <Antoniorioscarballo17@gmail.com>
2023-06-26 14:54:05 -0400

    added tuples notes

---
+++ python_fundamentals/python10_Tuple.py
@@ -0,0 +1,13 @@
+# Tuples
+# tuples are like lists, but you cannot modify their value
+# The initial values are the values that will stay for the rest of the program.
+# 
+# To declare a tuple, you write:
+ # tuple_name = (initial values) multiple values are separated by a comma
+
+# Example:
+months_of_year = ("jan", "feb", "mar", "apr", 'may', 'jun', 'july', 'aug', 'sep', 'oct', 'nov', 'dec')
+
+# you access the values of the tuple with their index values just like with a list
+# example:
+# months_of_year[0] = "jan", months_of_year[-1] = "dec" 
