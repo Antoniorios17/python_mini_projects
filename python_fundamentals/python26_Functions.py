fca04d62ef5ecdd7dc3166f94da32d1616a509e0
Antonio Rios <Antoniorioscarballo17@gmail.com>
2023-06-26 22:25:11 -0400

    python functions

---
+++ python_fundamentals/python26_Functions.py
@@ -0,0 +1,23 @@
+# Functions
+# Functions are simply prewritten code that perform a certain task
+
+#example
+#print(Hello World) will display Hello World
+
+#on the other hand
+# replace() function manipulates text strings
+string = "Hello World".replace("World", "Universe")
+print(string)
+# the string world gets replaced by Universe
+
+
+# Defining your own functions
+# we can define our own functions and reuse them in the program.
+# syntax
+
+# def function_name(list of parameters):
+#    code detailing what the function should do
+#    return [expression]
+
+# the are two keywords here, def and return
+
