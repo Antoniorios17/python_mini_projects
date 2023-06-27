4ab8330b888dd776603b6250813e65283a211816
Antonio Rios <Antoniorioscarballo17@gmail.com>
2023-06-27 17:50:02 -0400

    added remove rename functions

---
+++ python_fundamentals/python38_Deleting_renaming_files.py
@@ -0,0 +1,15 @@
+# Deleting and renaming files
+
+# Very useful functions are remove() and rename()
+# They are available in the os module
+
+# remove()  : deletes a file
+# syntax 
+# remove('filename')
+
+# rename()   : renames a file
+# syntax
+# rename('old_file_name', 'new_file_name')
+
+
+
