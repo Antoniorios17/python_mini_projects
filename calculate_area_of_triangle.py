57f92d9854058e0e2df5c7d8a2fbdcaeae7f168c
Antonio Rios <Antoniorioscarballo17@gmail.com>
2023-07-30 01:07:23 -0400

    added triangle calculator

---
+++ calculate_area_of_triangle.py
@@ -0,0 +1,11 @@
+# this program calculates the area of any triangle
+
+print("Welcome to the triangle area calculator")
+side1 = int(input("Please enter a number: "))
+side2 = int(input("Please enter another number:"))
+print("===================================")
+
+area = (side1 * side2)/2
+
+print(f"The area of the triangle is {area}")
+print("Thank you for using the triangle calculator")
