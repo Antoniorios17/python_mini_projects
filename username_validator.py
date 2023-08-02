11a218dceaa3e0c2f447a926a49722c95da0d9df
Antonio Rios <Antoniorioscarballo17@gmail.com>
2023-08-02 13:56:18 -0400

    updated usernaname validator

--- username_validator.py
+++ username_validator.py
@@ -6,7 +6,7 @@ def hint_username(username):
     if len(username) < 3 :
         print("Invalid username. Must be at least 3 charaters long")
     
-    elif len(username) > 15:
+    elif len(username) > 14:
         print("Invalid Username. Must be at most 14 characters long")
     else:
         print("Valid Username")
