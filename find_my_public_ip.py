5866c35706d925995c38d3bab350348e02ea80a4
Antonio Rios <Antoniorioscarballo17@gmail.com>
2023-08-13 23:07:46 -0400

    updated find my ip

--- find_my_public_ip.py
+++ find_my_public_ip.py
@@ -1,4 +1,14 @@
 from requests import get
 
-ip = get('https://api.ipify.org').content.decode('utf8')
-print('My public IP address is: {}'.format(ip))
+
+
+#find your public ip
+def find_ip():
+    ip = get('https://api.ipify.org').content.decode('utf8')
+    public_ip = 'My public IP address is: {}'.format(ip)
+    return public_ip
+
+while True:
+    user_input= input("Do you want to get your public IP: (yes/no) ").split()[0].lower()
+    if user_input == "y":
+        print(find_ip())
