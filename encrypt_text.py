c139e01f78660f3276f90491a1772a40a5179a3d
Antonio Rios <Antoniorioscarballo17@gmail.com>
2023-08-13 23:57:22 -0400

    updated encrytion program

--- encrypt_text.py
+++ encrypt_text.py
@@ -1,19 +1,32 @@
 import hashlib
 
 print("=================================")
-print("Welcome to the Encryption Creator")
+print("Welcome to the Encryption Machine")
 print("=================================\n\n")
-secret_message = 'Is is a super secret message'
-
-bsecret_message = secret_message.encode()
-
-# Creates Hashed Object
-m = hashlib.md5()
 
 
-m.update(bsecret_message)
 
-print(m.digest())
+def encryptor(message):
+    new_message = message.encode()
+    #creates a hash object
+    m= hashlib.md5()
+    encryption = m.update(new_message)
+    return encryption
 
-user_input = input("Please enter your message")
+
+
+
+while True:
+    user_input = input("Please enter your message: ")
+    if user_input != 0:
+        print(encryptor(user_input))
+    else:
+        print("Not a Valid input")
+        new_try = input("Would you like to try again?: (yes/no)")
+        if new_try == 'y':
+            continue
+        else:
+            print("")
+            print("Thank you, Goodbye")
+            print("")
 
