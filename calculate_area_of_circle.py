3126c4ed5bed712e9a4b0ea1360d1f93ba85482b
Antonio Rios <Antoniorioscarballo17@gmail.com>
2023-08-22 23:47:38 -0400

    updated circle

--- calculate_area_of_circle.py
+++ calculate_area_of_circle.py
@@ -3,7 +3,7 @@
 def circle_area(radius):
     pi = 3.14
     area = pi * radius
-    return f'The are of the circle is {area:.2f}'
+    return f'The are of your circle is {area:.2f}'
 
 # print(circle_area(5))
 # print(circle_area(10))
