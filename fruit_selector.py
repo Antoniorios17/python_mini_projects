fbd13ef0d80fe7b69ee42867d3f0814d3f93f411
Antonio Rios <Antoniorioscarballo17@gmail.com>
2023-08-29 22:48:47 -0400

    updated friut selector

--- fruit_selector.py
+++ fruit_selector.py
@@ -2,7 +2,7 @@ print("welcome to The Fruit Selector! ")
 
 entry = input("Select the first letter of your fruit: ")
 
-fruits = ["apple", "orange", "pineapple", "kiwi"]
+fruits = ["apple", "orange", "pineapple", "kiwi", "Blueberry"]
 
 def fruit_select(entry):
     for fruit in fruits:
