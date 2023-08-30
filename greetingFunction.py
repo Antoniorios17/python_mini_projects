57e09919688beb0bdd8bf021d750aebbb244cfad
Antonio Rios <Antoniorioscarballo17@gmail.com>
2023-08-29 23:58:16 -0400

    updated greeting

--- greetingFunction.py
+++ greetingFunction.py
@@ -7,13 +7,13 @@ def greeting(input):
 print("Welcome to the greeting program:\n ")
 
 while True:
-    user_input = input('''Please enter your name \nOr type exit to leave: \n''')
+    user_input = input('''Please enter your name: \nOr type exit to leave: \n''')
     if user_input == "exit":
         break
     else:
         print(greeting(user_input))
         response = input("Would you like to continue? ")
-        if response[0].split().lower() == "y":
+        if response[0].lower() == "y":
             continue
         else:
             break
