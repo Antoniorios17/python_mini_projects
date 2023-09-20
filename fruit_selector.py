93aa86fdd25b64a6b3c3ddbfafbfdf23b8ae04c4
Antonio Rios <Antoniorioscarballo17@gmail.com>
2023-09-19 22:43:48 -0400

    edited fruit_selector

--- fruit_selector.py
+++ fruit_selector.py
@@ -2,11 +2,11 @@ print("welcome to The Fruit Selector! ")
 
 entry = input("Select the first letter of your fruit: ")
 
-fruits = ["apple", "orange", "pineapple", "kiwi", "Blueberry"]
+fruits = ["apple", "orange", "pineapple", "kiwi", "Blueberry", "cherry"]
 
 def fruit_select(entry):
     for fruit in fruits:
-        if fruit[0] == entry:
+        if fruit[0].lower() == entry:
             response = f'The fruit that start with {entry} is {fruit}'
         return response
 
